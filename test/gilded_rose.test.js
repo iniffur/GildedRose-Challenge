199229a3cb341dc4b0e7ab9aaee5344ca630f853
iniffur <jamesruffini97@gmail.com>
2022-10-26 13:45:10 +0100

    Update Normal Items Function Added

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,22 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", () =>  {
+  it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    const items = gildedRose.newUpdateQuality();
+    expect(items[0].name).toBe("foo");
   });
+
+  it("normal items degrade by one with each passing day", () => {
+    const gildedRose = new Shop([new Item("foo", 10, 10)]);
+    const items = gildedRose.newUpdateQuality();
+    expect(items[0].quality).toBe(9);
+    expect(items[0].sellIn).toBe(9);
+  })
+
+  it("quality degrades twice as fast once sell by date has passed for normal items", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 10)]);
+    const items = gildedRose.newUpdateQuality();
+    expect(items[0].quality).toBe(8);
+  })
 });
